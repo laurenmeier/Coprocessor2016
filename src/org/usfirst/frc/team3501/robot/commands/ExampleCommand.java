7023ec53eeb348399dd6e651b174df613d6501a3
Lauren Meier <meier.lauren@gmail.com>
2017-03-20 10:43:33 -0700

    add comments to specify code function and remove unnecessary messages

--- src/org/usfirst/frc/team3501/robot/commands/ExampleCommand.java
+++ src/org/usfirst/frc/team3501/robot/commands/ExampleCommand.java
@@ -3,37 +3,38 @@ package org.usfirst.frc.team3501.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-import org.usfirst.frc.team3501.robot.Robot;
-
 /**
  *
  */
 public class ExampleCommand extends Command {
 
-    public ExampleCommand() {
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.exampleSubsystem);
-    }
+  public ExampleCommand() {
+  }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
 }
